2211ca7df7bc6912dd6a3a0e4051fe2b30ec1666
JoacoMaurtua <joacomaurtua@gmail.com>
2021-10-05 21:32:48 -0500

    animate function implemented

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-
-let canvasElement = document.getElementById("My-Canvas");
+//Ejemplo de canvas
+/* let canvasElement = document.getElementById("My-Canvas");
 let canvasContext = canvasElement.getContext("2d");// se carga el plano
 
 canvasContext.beginPath();
@@ -8,6 +8,34 @@ canvasContext.lineWidth = 10;
 canvasContext.strokeStyle = 'blue';
 canvasContext.strokeRect(10, 10, 70,70);
 canvasContext.fillStyle = 'red';
-canvasContext.fillRect(40,30,30,35)//coordX, coordY, ancho, alto
+canvasContext.fillRect(40,30,30,35)//coordX, coordY, ancho, alto */
+
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+
+//Asegurar las medidas del lienzo
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+
+const playerImage = new Image(); //propiedad para insertar una imagen
+playerImage.src = 'shadow_dog.png';
 
 
+const animate =()=>{
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx.fillRect(50,50,100,100);
+  requestAnimationFrame(animate); //Que la imagen se repita automaticamente
+
+};
+
+animate();
+
+
+
+
+
+// ancho --> 6876
+
+// height --> 5230
+
+// Cada fotograma mide 573
